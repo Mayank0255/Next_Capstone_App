3b0b1c24f6e121fe51deec7828ed6587a7a9dcee
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2021-02-14 22:53:29 +0530

    feat: add Header component and styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react';
+import Nav from './Nav';
+import Header from './Header';
 import styles from '../styles/Layout.module.css'
-import Nav from "./Nav";
 
 const Layout = ({ children }) => {
     return <Fragment>
         <Nav/>
         <div className={styles.container}>
             <main className={styles.main}>
+                <Header/>
                 {children}
             </main>
         </div>
