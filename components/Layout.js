9d459302a1d05ff790a504a2faef3f4d05b778d9
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2021-02-14 22:48:36 +0530

    feat: add NavBar

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,16 @@
+import React, {Fragment} from 'react';
 import styles from '../styles/Layout.module.css'
+import Nav from "./Nav";
 
 const Layout = ({ children }) => {
-    return (
+    return <Fragment>
+        <Nav/>
         <div className={styles.container}>
             <main className={styles.main}>
                 {children}
             </main>
         </div>
-    )
+    </Fragment>
 }
 
 export default Layout
